da04fda4eea0fa40b51a9245ec8d1ab35491a31c
Jianbai <jianbaiye@outlook>
2022-06-07 17:28:40 +0800

    add supp1

--- supps/supp1.py
+++ supps/supp1.py
@@ -21,14 +21,53 @@ core_cols = ['Dataset', "Sub-type", "#Maxquant 100% FDR PSMs", "#Filtered 100% P
 core_data = []
 
 # --------------
+regenerated_those_three = False
+if regenerated_those_three:
+    dataset = "Bekker-Jensen"
+    frag_model = 'prosit_l1'
+    for which in ["trypsin", 'chymo', "lysc", "gluc"]:
+        msms_file = f"/data/prosit/figs/fig235/{which}/maxquant/combined/txt/msms.txt"
+        save_tab1 = f"/data/prosit/figs/fig235/{which}/percolator_up/try/{frag_model}/no_finetuned_twofold/sa.tab"
+        # save_tab2 = f"/data/prosit/figs/fig235/{which}/percolator_up/try/{frag_model}/finetuned_twofold/sa.tab"
+        counts = counting(msms_file, save_tab1)
+        core_data.append((
+            dataset, which, *counts
+        ))
 
-dataset = "Bekker-Jenses"
-frag_model = 'prosit_l1'
-for which in ["trypsin", 'chymo', "lysc", "gluc"]:
-    msms_file = f"/data/prosit/figs/fig235/{which}/maxquant/combined/txt/msms.txt"
-    save_tab1 = f"/data/prosit/figs/fig235/{which}/percolator_up/try/{frag_model}/no_finetuned_twofold/sa.tab"
-    save_tab2 = f"/data/prosit/figs/fig235/{which}/percolator_up/try/{frag_model}/finetuned_twofold/sa.tab"
-    counts = counting(msms_file, save_tab1, save_tab2)
+    # ------------------
+    dataset = "Davis"
+    frag_model = 'prosit_l1'
+    msms_file = f"/data/prosit/figs/figure5/maxquant/combined/txt/msms.txt"
+    save_tab1 = save_tab1 = f"/data/prosit/figs/figure5/percolator/try/{frag_model}/no_finetuned_twofold/sa.tab"
+    # save_tab2 = f"/data/prosit/figs/fig235/{which}/percolator_up/try/{frag_model}/finetuned_twofold/sa.tab"
+    counts = counting(msms_file, save_tab1)
     core_data.append((
-        dataset, which, *counts
+        dataset, "", *counts
     ))
+    # ---------------------
+    dataset = 'Metaproteomics'
+    frag_model = 'prosit_l1'
+    for which in ["sprot_human", "IGC", "sprot_all", "sprot_bacteria_human"]:
+        msms_file = f"/data/prosit/figs/figure6/{which}/maxquant/txt/msms.txt"
+        save_tab1 = f"/data/prosit/figs/figure6/{which}/percolator/try/{frag_model}/no_finetuned_twofold/sa.tab"
+        counts = counting(msms_file, save_tab1)
+        core_data.append((
+            dataset, which, *counts
+        ))
+else:
+    core_data.extend([
+        ("Bekker-Jensen", 'trypsin', 761563, 663031, 502736, 160295),
+        ("Bekker-Jensen", 'chymo', 990431, 921917, 596067, 325850),
+        ("Bekker-Jensen", 'lysc', 834749, 714865, 500689, 214176),
+        ("Bekker-Jensen", 'gluc', 898603, 774544, 514496, 260048),
+        ('Davis', '', 1148358, 141702, 105864, 35838),
+        ('Metaproteomics', 'sprot_human', 355909, 337862, 175446, 162416),
+        ('Metaproteomics', 'IGC', 358491, 342049, 228140, 113909),
+        ('Metaproteomics', 'sprot_all', 351263, 334216, 187417, 146799),
+        ('Metaproteomics', 'sprot_bacteria_human', 346649, 329605, 183636, 145969)
+    ])
+
+
+core_pd = pd.DataFrame(columns=core_cols, data=core_data)
+print(core_pd.head())
+core_pd.to_csv("data/supp1.csv", index=False)
